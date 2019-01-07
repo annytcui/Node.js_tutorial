4e90bb4c718d26874ec55f7638407d7f5806ea3f
tianyuancuipan <tianyuan.cui.sc@gmail.com>
2019-01-07 10:42:48 -0600

    Add demo_createfile

--- demo_createfile.js
+++ demo_createfile.js
@@ -1,6 +1,18 @@
 var fs = require('fs');
-
+// fs.appendFile()
 fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+// fs.open()
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+// fs.writeFile()
+fs.writeFile('mynewfile3.txt', 'Hello Content!', function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
